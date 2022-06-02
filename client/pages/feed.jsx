a55ce31adfcdbdeceeffe0500653bc8c4d189bfd
Nick Newton <nickanewton@gmail.com>
2022-06-02 22:40:53 +0000

    Built front end functionality.

--- client/pages/feed.jsx
+++ client/pages/feed.jsx
@@ -1,15 +1,41 @@
 import React from 'react';
 
 class Feed extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('/api/feed')
+      .then(res => res.json())
+      .then(posts => this.setState({ posts }));
+  }
+
   render() {
     return (
       <>
         <div className='container'>
           <ul className='d-flex flex-wrap mt-3 mb-3 list-unstyled justify-content-around width-440 mx-auto'>
-            <li className='mb-3 d-flex align-items-center'>
-              <img src="/images/image-1654108361874.webp" alt="" />
+            {
+              this.state.posts.map(posts => (
+            <li key={posts.postId} id={posts.postId} className='mb-3 d-flex align-items-center'>
+              <img src={posts.imageUrl} alt="" />
             </li>
-            <li className='mb-3 d-flex align-items-center'>
+              ))
+            }
+          </ul>
+        </div>
+      </>
+    );
+  }
+}
+
+export default Feed;
+
+/* <li className='mb-3 d-flex align-items-center'>
               <img src="/images/image-1654036710760.jpeg" alt="" />
             </li>
             <li className='mb-3 d-flex align-items-center'>
@@ -23,12 +49,4 @@ class Feed extends React.Component {
             </li>
             <li className='mb-3 d-flex align-items-center'>
               <img src="/images/image-1654036586244.jpeg" alt="" />
-            </li>
-          </ul>
-        </div>
-      </>
-    );
-  }
-}
-
-export default Feed;
+            </li> */

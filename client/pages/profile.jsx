0ba9fbf5b991375b3032036a2cff979a52a8a578
Nick Newton <nickanewton@gmail.com>
2022-07-02 20:42:36 +0000

    Used context to get username.

--- client/pages/profile.jsx
+++ client/pages/profile.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import AppContext from '../lib/app-context';
 
 export default class Profile extends React.Component {
   render() {
+    const { user } = this.context;
     return (
       <div className="mx-auto container content-width">
         <div className="row">
           <div className="col d-flex justify-content-center align-items-center">
-            <div>
+            <div className="text-center mt-5">
               <img src="/images/prof2.png" alt="" className='bg-info' />
-              <h2 className='text-info'>UserName</h2>
+              <h2 className='text-info mt-4'>{user.username}</h2>
             </div>
           </div>
         </div>
@@ -16,3 +18,5 @@ export default class Profile extends React.Component {
     );
   }
 }
+
+Profile.contextType = AppContext;

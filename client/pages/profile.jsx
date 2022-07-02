2d5624c01ab9900a5bd2db34fc99c0d104487a99
Nick Newton <nickanewton@gmail.com>
2022-07-02 20:30:51 +0000

    Added profile image

--- client/pages/profile.jsx
+++ client/pages/profile.jsx
@@ -3,14 +3,12 @@ import React from 'react';
 export default class Profile extends React.Component {
   render() {
     return (
-      <div>
+      <div className="mx-auto container content-width">
         <div className="row">
-          <div className="col">
+          <div className="col d-flex justify-content-center align-items-center">
             <div>
-              <img src="" alt="" />
-            </div>
-            <div>
-              <h2>UserName</h2>
+              <img src="/images/prof2.png" alt="" className='bg-info' />
+              <h2 className='text-info'>UserName</h2>
             </div>
           </div>
         </div>

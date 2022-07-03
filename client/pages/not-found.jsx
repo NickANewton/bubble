0f078cafd1d612d60ea16835546dab0b67a15575
Nick Newton <nickanewton@gmail.com>
2022-07-03 21:55:51 +0000

    Added Not found page.

---
+++ client/pages/not-found.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function NotFound() {
+  return (
+    <div>
+      <div className="row">
+        <div className="col">
+          <p>Uh oh! We could not find the page you are looking for!</p>
+          <a href="#">Return Home</a>
+        </div>
+      </div>
+    </div>
+  );
+}
